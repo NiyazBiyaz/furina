77186617b461e3d822d6c421baa0fcba09716844
NiyazBiyaz <niyazik114422@gmail.com>
2025-04-08 20:27:53 +0500

    Change `level` creating and add `Level` class

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
 
 
-LEVEL = (
-    "1 1 1 1 1 1 1 1",
-    "1 0 0 1 0 0 0 1",
-    "1 0 1 0 1 1 0 1",
-    "1 0 1 0 0 1 0 1",
-    "1 0 0 0 0 0 0 1",
-    "1 1 1 1 1 1 1 1",
-)
+LEVEL = \
+"""1 1 1 1 1 1 1 1
+1 0 0 1 0 0 0 1
+1 0 1 0 1 1 0 1
+1 0 1 0 0 1 0 1
+1 0 0 0 0 0 0 1
+1 1 1 1 1 1 1 1
+"""
 CHUNK_SIZE = 100
 
 
@@ -35,31 +35,7 @@ def main():
     player = Player(pg.Surface((50, 50)), pg.Rect(400, 300, 50, 50), 2)
     players_group.add(player)
 
-
-    # Инициализация уровня
-    ground = pg.sprite.Group() # Группа для чанков земли
-    walls  = pg.sprite.Group() # Группа для чанков стен
-    for y, line in enumerate(LEVEL):
-        for x, chunk in enumerate(line.split()):
-            if chunk == "0":
-                chunk = pg.sprite.Sprite(ground) # Чанк `0` - земля
-                # Инициализация спрайта
-                chunk.image = pg.Surface((CHUNK_SIZE, CHUNK_SIZE))
-                chunk.rect  = chunk.image.get_rect()
-                chunk.rect.topleft = x * CHUNK_SIZE, y * CHUNK_SIZE
-
-                chunk.image.fill(GREEN) # Цвет зеленый
-
-            elif chunk == "1":
-                chunk = pg.sprite.Sprite(walls) # Чанк `1` - стены 
-                # Инициализация спрайта
-                chunk.image = pg.Surface((CHUNK_SIZE, CHUNK_SIZE))
-                chunk.rect  = chunk.image.get_rect()
-                chunk.rect.topleft = x * CHUNK_SIZE, y * CHUNK_SIZE
-
-                chunk.image.fill(RED) # Цвет красный
-    
-    obstacles = pg.sprite.Group(walls.sprites())
+    level = Level(LEVEL, CHUNK_SIZE)
 
     # Gameloop helpers
     clock = pg.time.Clock()
@@ -71,13 +47,13 @@ def main():
             if event.type == pg.QUIT:
                 running = False
         keys = pg.key.get_pressed()
-        players_group.update(keys, obstacles)
+        players_group.update(keys, level.obstacles)
 
         # Rendering
         screen.fill(WHITE)
         
-        ground.draw(screen)
-        walls. draw(screen)
+        level.ground.draw(screen)
+        level.walls. draw(screen)
         players_group.draw(screen)
 
         pg.display.flip()
@@ -85,6 +61,35 @@ def main():
         clock.tick(FPS)
 
     pg.quit()
+
+
+class Level:
+
+    def __init__(self, map: str, chunk_size):
+        # Инициализация уровня
+        self.ground = pg.sprite.Group() # Группа для чанков земли
+        self.walls  = pg.sprite.Group() # Группа для чанков стен
+        for y, line in enumerate(map.split("\n")):
+            for x, chunk in enumerate(line.split()):
+                if chunk == "0":
+                    chunk = pg.sprite.Sprite(self.ground) # Чанк `0` - земля
+                    # Инициализация спрайта
+                    chunk.image = pg.Surface((chunk_size, chunk_size))
+                    chunk.rect  = chunk.image.get_rect()
+                    chunk.rect.topleft = x * chunk_size, y * chunk_size
+
+                    chunk.image.fill(GREEN) # Цвет зеленый
+
+                elif chunk == "1":
+                    chunk = pg.sprite.Sprite(self.walls) # Чанк `1` - стены 
+                    # Инициализация спрайта
+                    chunk.image = pg.Surface((chunk_size, chunk_size))
+                    chunk.rect  = chunk.image.get_rect()
+                    chunk.rect.topleft = x * chunk_size, y * chunk_size
+
+                    chunk.image.fill(RED) # Цвет красный
+            
+        self.obstacles = pg.sprite.Group(self.walls.sprites())
 
 
 # Класс игрока
@@ -111,7 +116,6 @@ class Player(pg.sprite.Sprite):
         if pg.sprite.spritecollideany(self, obastacles):
             self.rect.left = prev_left
         
-        # делаем движение, если оно заставляет нас столкнуться с препятствием возвращаем прошлое
         self.rect.centery += self._move_y
         if pg.sprite.spritecollideany(self, obastacles):
             self.rect.top = prev_top
